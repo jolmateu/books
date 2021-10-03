5fa3dc208418ba22f5eb010b47d148817af30f23
jolmateu <jolmateu@byui.edu>
2021-10-03 00:44:42 -0500

    Week 03
    Week 03

--- routes/shop.js
+++ routes/shop.js
@@ -2,21 +2,18 @@ const path =  require('path');
 
 const express = require('express');
 
-const rootDir = require('../util/path');
-const adminData = require('./admin');
+const shopController = require('../controllers/shop');
 
 const router = express.Router();
 
-router.get('/', (req, res, next) => {
-  const books = adminData.books;
-  res.render('shop', {
-    bks: books,
-    pageTitle: 'Shop',
-    path: '/',
-    hasBooks: books.length > 0,
-    activeShop: true,
-    booksCSS: true
-  });
-});
+router.get('/', shopController.getIndex);
+
+router.get('/products', shopController.getProducts);
+
+router.get('/cart', shopController.getCart);
+
+router.get('/orders', shopController.getOrders);
+
+router.get('/checkout', shopController.getCheckout);
 
 module.exports = router;
